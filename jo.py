251421d77179a4e05dbc76de8bc62a667e645d89
Josselin Davion Jouffre <josselin.davion--jouffre@etu.minesparis.psl.eu>
2026-01-30 11:05:42 +0100

    premiere version des classes

--- jo.py
+++ jo.py
@@ -27,6 +27,31 @@ class Sheep:
         self.energy += energy_gain
     
     ## Méthode pour tuer le mouton ##
-    def isalive(self):
+    def is_alive(self):
         if self.energy <= 0 or self.age >= SHEEP_MAX_AGE:
             self.alive = False
+
+
+## CLASSE DE L'HERBE ##
+class Grass:
+
+    ## Méthode d'initialisation de la classe Grass ##
+    def __init__(self, position:tuple, age:int, alive:bool):
+        self.age = age
+        self.position = position
+        self.alive = alive
+    
+    ## Méthode pour afficher les informations de l'objet Grass ##
+    def __repr__(self):
+        return f'Grass(position={self.position}, age={self.age}, alive={self.alive})'
+    
+    ## Méthode du temps de repousse ##
+    ## On fait un grow_back à chaque tour sur celles qui sont mortes ##
+    def grow_back(self):
+        self.age += 1
+        if self.age >= GRASS_GROWTH_TIME:
+            self.alive = True
+            self.age = 0
+
+    def die(self):
+        self.alive = False
