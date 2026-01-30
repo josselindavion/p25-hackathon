2854687a6a9a7f37ee3fd7f589c53b39c50f8317
Josselin Davion Jouffre <josselin.davion--jouffre@etu.minesparis.psl.eu>
2026-01-30 10:32:24 +0100

    chgmt

--- jo.py
+++ jo.py
@@ -1,13 +1,13 @@
-
 ## CLASSE DU MOUTON ##
 class Sheep:
     
     ## Méthode d'initialisation de la classe Sheep ##
-    def __init__(self, id:int, position:tuple, energy:int, age:int):
+    def __init__(self, id:int, position:tuple, energy:int, age:int, alive:bool):
         self.id = id
         self.position = position
         self.energy = energy
         self.age = age
+        self.alive = alive
     
     ## Méthode pour afficher les informations de l'objet Sheep ##
     def __repr__(self):
@@ -17,5 +17,16 @@ class Sheep:
     def aging_sheep(self):
         self.age += 1
     
+    ## Méthode pour actualiser la position du mouton ##
     def deplace(self, new_position:tuple):
         self.position = new_position
+        self.energy -= 1  ## Déplacement coûte de l'énergie
+    
+    ## Méthode pour actualiser l'énergie du mouton #
+    def energy_gain(self, energy_gain:int):
+        self.energy += energy_gain
+    
+    ## Méthode pour tuer le mouton ##
+    def isalive(self):
+        if self.energy <= 0 or self.age >= SHEEP_MAX_AGE:
+            self.alive = False
