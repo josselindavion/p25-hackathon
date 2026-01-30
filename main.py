83940993e3ae8c0c546ea61f3956ce4d0811feb1
Josselin Davion Jouffre <josselin.davion--jouffre@etu.minesparis.psl.eu>
2026-01-30 14:30:46 +0100

    fct main

--- main.py
+++ main.py
@@ -35,3 +35,11 @@ GRASS_REGROWTH_TIME = 7
 # --- Paramètres de la Simulation ---
 MAX_TURNS = 500  # Nombre maximum de tours
 
+def main():
+    ## Initialisation de la grille ##
+    grid = Grid(GRID_SIZE[0], GRID_SIZE[1])
+    grid.initialisation(INITIAL_SHEEP, INITIAL_WOLVES)
+    for tour in range(MAX_TURNS):
+        print(f"Tour {tour + 1}")
+        grid.evolve() 
+    print("Simulation terminée.")
