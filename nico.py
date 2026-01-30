2eaae9d28bb748e9bb2103979d207e8ea100abc1
Nicolas Demore <nicolas.demore@etu.minesparis.psl.eu>
2026-01-30 10:50:15 +0100

    début nico

---
+++ nico.py
@@ -0,0 +1,36 @@
+class Wolf:
+
+    #paramètres initiales de la classe.
+    def __init__(self, id: int, position: tuple, energy: int, age: int, alive: bool):
+        self.id = id
+        self.position = position
+        self.energy = energy
+        self.age = age
+        self.alive = alive
+
+    def __repr__(self):
+        return f"Wolf(id={self.id}, position=({self.position}), energy={self.energy}, age={self.age})"
+    
+    #prend 1 an en plus au bout d'un tour
+    def aging_wolf(self):
+        self.age += 1
+
+    #déplace le loup à une nouvelle position
+    def deplace(self, new_position):
+        self.position = new_position
+
+    #augmente/diminue le niveau d'énergie du loup
+    def energy_gain(self, energy_gain: int):
+        self.energy += energy_gain
+
+    # Méthode pour savoir si le loup est en vie
+    def is_alive(self):
+        if self.energy <= 0 or WOLF_MAX_AGE >= self.age: #WOLF_MAX_AGE sera défini quelque part dans le code principal
+            self.alive = False
+    
+
+
+
+
+w = Wolf(1, (10, 20), 100, 5)
+print(w)  
